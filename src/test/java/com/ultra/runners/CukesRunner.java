5c4738037a1b4dbfe17a9a2e4f004937caeff039
Gokhan Serin <penmudamu@gmail.com>
2023-09-11 14:58:26 +0300

    CukesRunner added

---
+++ src/test/java/com/ultra/runners/CukesRunner.java
@@ -0,0 +1,31 @@
+package com.ultra.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        plugin = {
+                //"pretty",
+                "html:target/cucumber-report.html",
+                "json:target/cucumber-report.json",
+                "junit:target/cucumber-report.xml",
+                "rerun:target/rerun.txt",
+
+
+        },
+        features = "src/test/resources/features",
+        glue = "com/ultra/step_definitions",
+
+        dryRun = false,
+        tags = "",
+        //stepNotifications = true,
+        publish = true
+
+
+)
+public class CukesRunner {
+}
+
